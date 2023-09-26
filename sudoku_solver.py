c249e3c8f828ad59dabf1f61baf2254df9b5ce9b
HarryEP <44836842+HarryEP@users.noreply.github.com>
2023-09-26 17:01:45 +0100

    get coordinates

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -23,31 +23,40 @@ test_sudoku = np.array([["x", "x", 4, "x", 5, "x", "x", "x", "x"],
                         ["x", "x", "x", "x", 6, "x", 1, "x", "x"]], dtype='object')
 
 
-def check_three_by_three(box: np.array, value: int) -> bool:
+def check_box(box: np.array, value: int) -> bool:
     """Checks that the value is available for that three by three box"""
-    pass
+    return True
 
 
-def check_row(row: np.array, value: int) -> bool:
-    """Checks that the value is available for that row"""
-    return not value in row
+def check_line(line: np.array, value: int) -> bool:
+    """Checks that the value is available for that row/column"""
+    return not value in line
 
 
-def check_column(column: np.array, value: int) -> bool:
-    """Checks that the value is available for that column"""
-    pass
+def check_all_conditions(row: np.array, column: np.array, box: np.array, value: int):
+    """Checks that all 3 checks are valid"""
+    return all([check_line(row, value), check_line(column, value),
+               check_box(box, value)])
+
+
+def get_position(x):
+    result = (x // 3) * 3
+    return [result, result + 1,  result + 2]
 
 
 if __name__ == "__main__":
     fake = np.empty((9, 9))
     "for every 1-9, check every row"
-    for i in range(1, 2):
-        for j in range(1, 2):
+    for i in range(9):
+        for j in range(9):
             for k in range(1, 10):
-                if test_sudoku[i-1, j-1] == "x":
+                if test_sudoku[i, j-1] == "x":
+                    row = test_sudoku[i, :]
+                    column = test_sudoku[:, j]
+                    coordinates = (get_position(i), get_position(j))
                     print(
-                        check_row(test_sudoku[i-1, :], k), test_sudoku[i-1, :], i, j, k)
-                    # check col
+                        check_line(row, k), row, i, j, k)
+                    check_line(column, k)
                     # check box
                 else:
                     pass
