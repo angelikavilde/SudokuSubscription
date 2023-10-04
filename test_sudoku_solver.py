1e6582e566b3b229c6aa46a6bd6ed0d8b5c4b5e1
Angela Vilde <angelikavilde@gmail.com>
2023-10-04 19:29:20 +0100

    added test for check_line function

--- test_sudoku_solver.py
+++ test_sudoku_solver.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-from sudoku_solver import check_box
+from sudoku_solver import check_box, check_line
 
 
 def test_check_box():
@@ -11,6 +11,17 @@ def test_check_box():
     returning bool whether it can or not add
     the value in following the sudoku rules
     """
-    box = np.array([[2,4],[1,9]])
+    box = np.array([[2, 4], [1, 9]], dtype="object")
     assert not check_box(box, 1)
     assert check_box(box, 8)
+
+
+def test_check_line():
+    """
+    Verifies that function returns correct
+    bool value based on whether a number can
+    be placed into the array
+    """
+    line = np.array([1, 2, "x"], dtype="object")
+    assert check_line(line, 5)
+    assert not check_line(line, 2)
